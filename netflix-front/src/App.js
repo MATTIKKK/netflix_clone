931b17b1e556ce1e959bda2dbdb706c0d26f04f3
Your Name <you@example.com>
2024-08-08 19:58:06 +0500

    home page 1 change

---
+++ netflix-front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./app.scss"
+import Home from './home/Home'
+
+const App = () => {
+  return (
+    <div>
+        <Home />
+    </div>
+  )
+}
+
+export default App
